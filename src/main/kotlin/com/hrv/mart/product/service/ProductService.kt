f03adf4886982ecaf040264bcd8afab2fc4df52e
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-21 16:12:17 +0530

    Remove unused import from Product Service

--- src/main/kotlin/com/hrv/mart/product/service/ProductService.kt
+++ src/main/kotlin/com/hrv/mart/product/service/ProductService.kt
@@ -1,6 +1,5 @@
 package com.hrv.mart.product.service
 
-import com.hrv.mart.product.model.Product
 import com.hrv.mart.product.repository.ProductRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
