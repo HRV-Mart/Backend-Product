5a127becd148b5e34896b711165f89ceceef3c19
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-21 22:29:40 +0530

    Minor change in Product model

--- src/main/kotlin/com/hrv/mart/product/model/Product.kt
+++ src/main/kotlin/com/hrv/mart/product/model/Product.kt
@@ -4,7 +4,7 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document
+@Document("Product")
 data class Product (
     val name: String,
     val description: String,
