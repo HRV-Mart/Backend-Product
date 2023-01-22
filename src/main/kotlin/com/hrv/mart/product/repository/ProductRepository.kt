406851faa8377d3f7272c49d1426496a9abcd706
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-01-22 22:56:07 +0530

    Change in repository

--- src/main/kotlin/com/hrv/mart/product/repository/ProductRepository.kt
+++ src/main/kotlin/com/hrv/mart/product/repository/ProductRepository.kt
@@ -1,8 +1,14 @@
 package com.hrv.mart.product.repository
 
 import com.hrv.mart.product.model.Product
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Repository
-interface ProductRepository : ReactiveMongoRepository<Product, String>
+interface ProductRepository : ReactiveMongoRepository<Product, String> {
+    fun findProductsByNameNotNull(pageRequest: PageRequest): Flux<Product>
+    fun countProductByNameNotNull(): Mono<Long>
+}
