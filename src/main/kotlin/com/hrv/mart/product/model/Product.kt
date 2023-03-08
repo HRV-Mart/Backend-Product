51cd79c22cad90ce6629c8f27e8a560003734f56
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-08 09:15:04 +0530

    Added price in product model

--- src/main/kotlin/com/hrv/mart/product/model/Product.kt
+++ src/main/kotlin/com/hrv/mart/product/model/Product.kt
@@ -8,12 +8,13 @@ import org.springframework.data.mongodb.core.mapping.Document
 data class Product(
     val name: String,
     val description: String,
-    val images: List<String>
+    val images: List<String>,
+    val price: Long
 ) {
     @Id
     var id: String = ObjectId().toString()
-    constructor(name: String, description: String, images: List<String>, id: String) :
-        this(name, description, images) {
+    constructor(name: String, description: String, images: List<String>, price:Long, id: String) :
+        this(name, description, images, price) {
         this.id = id
     }
     fun setIdToDefault(): Product {
