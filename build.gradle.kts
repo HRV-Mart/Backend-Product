d85641ecf7928210838029b2639af37b4bbc608a
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-03-22 13:00:29 +0530

    Remove product model and added product library (#39)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.22.0")
     // Custom-Pageable
     implementation("com.hrv.mart:custom-pageable:0.0.1-SNAPSHOT")
+    // Product Model
+    implementation("com.hrv.mart:product:0.0.1")
 }
 
 tasks.withType<KotlinCompile> {
